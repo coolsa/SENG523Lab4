12d9f035bcc25cb9e96beb5162d4153f1232e3fe
Trevor Brown <59982025+trevorbrown18@users.noreply.github.com>
2021-11-29 17:13:35 -0700

    Add files via upload
    Shell for MVC packages, main in the controller package.

---
+++ SENG523Lab4/src/controller/ATMProject.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class ATMProject {
+	
+	public static void main(String[] Args) {
+		System.out.println("Eventually main will display the Welcome Screen on start.");
+	}
+	
+	
+}
