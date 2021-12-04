ec61df168dcc0e27ee57f38ddacbf37e75ccda50
kaziashfaq <40367281+kaziashfaq@users.noreply.github.com>
2021-12-03 17:18:39 -0700

    Added verifying card feature

--- SENG523Lab4/src/controller/WelcomeFrameController.java
+++ SENG523Lab4/src/controller/WelcomeFrameController.java
@@ -5,39 +5,47 @@ import java.awt.event.ActionListener;
 
 import view.EnterPinScreen;
 import view.WelcomeScreen;
+import model.BankCard;
+import model.CardScanner;
+import model.CardDatabase;
 
+import static javax.swing.JOptionPane.showMessageDialog;
 
 //Todo:  add logic into the controller - error check for invalid card, move to withdraw screen for valid. 
 public class WelcomeFrameController {
 
 	private WelcomeScreen welcomeScreen;
-	
-	
+	private CardScanner cardScanner;
+	private CardDatabase cardDB;
+
 	public WelcomeFrameController(WelcomeScreen welcomeScreen) {
 		this.welcomeScreen = welcomeScreen;
-
+		cardScanner = new CardScanner(welcomeScreen.getScb());
+		cardDB = welcomeScreen.getCardDB();
 	}
-	
-	//Action Listener Events to be added here. 
+
+	// Action Listener Events to be added here.
 	public void runController() {
-		
+
 		//
 		welcomeScreen.getInsertCardButton().addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(welcomeScreen.getCardNumber().getText());
-				EnterPinScreen enterPinScreen = new EnterPinScreen();
-				enterPinScreen.displayFrame();
-				
+				// System.out.println(welcomeScreen.getCardNumber().getText());
+				int card = Integer.parseInt(welcomeScreen.getCardNumber().getText());
+				BankCard bc = cardDB.getCard(card);
+				if (bc.getCardNumber() == 0000 && bc.getBalance() == 0000 && bc.isAccountStatus() == false) {
+					showMessageDialog(null, "The card is not valid");
+					welcomeScreen.reset();
+				} else {
+					EnterPinScreen enterPinScreen = new EnterPinScreen();
+					enterPinScreen.displayFrame();
+				}
 			}
-			
-			
+
 		});
-		
-		
+
 	}
-	
-	
-	
+
 }
--- SENG523Lab4/src/model/CardScanner.java
+++ SENG523Lab4/src/model/CardScanner.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class CardScanner {
 
-	// Holds an instance of SCB to notify SCB of state cardInserted. 
+	// Holds an instance of SCB to notify SCB of state cardInserted.
 	private SCB scb;
-	private CardDatabase cardDB; 
+	private CardDatabase cardDB;
 	private boolean cardStatus; // T = Valid, F = Invalid
 	private boolean ejected;
-	
+
 	public CardScanner(SCB scb) {
 		this.setScb(scb);
 	}
-	
+
 	public SCB getScb() {
 		return scb;
 	}
@@ -37,26 +37,25 @@ public class CardScanner {
 	public void setEjected(boolean ejected) {
 		this.ejected = ejected;
 	}
-	
-	
-	// Quick and dirty way to check our mock DB cards. Good enough for now since 
-	// We don't have an actual DB and will only hold a few example cards. 
+
+	// Quick and dirty way to check our mock DB cards. Good enough for now since
+	// We don't have an actual DB and will only hold a few example cards.
 	public boolean scannedCardIsValid(BankCard insertedCard, int pin) {
 		ArrayList<BankCard> cardHolders = cardDB.getCardHolders();
-		
-		for(int i = 0; i< cardHolders.size(); i ++) {
-			if(insertedCard.getCardNumber() == cardHolders.get(i).getCardNumber()) {
-				if(pin == insertedCard.getPinNumber()) {
+
+		for (int i = 0; i < cardHolders.size(); i++) {
+			if (insertedCard.getCardNumber() == cardHolders.get(i).getCardNumber()) {
+				if (pin == insertedCard.getPinNumber()) {
 					setEjected(false);
 					setCardStatus(true);
-					return true; 
+					return true;
 				}
 			}
 		}
-		
+
 		setCardStatus(false);
 		setEjected(true);
 		return false;
 	}
-	
+
 }
