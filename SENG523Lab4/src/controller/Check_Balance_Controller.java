14568edf818ffc15ab2f1d31550ffef954018c1d
kaziashfaq <40367281+kaziashfaq@users.noreply.github.com>
2021-12-05 10:06:07 -0700

    Added deposit feature

--- SENG523Lab4/src/controller/Check_Balance_Controller.java
+++ SENG523Lab4/src/controller/Check_Balance_Controller.java
@@ -11,6 +11,7 @@ import model.CardDatabase;
 import view.Check_Balance;
 
 import static javax.swing.JOptionPane.showMessageDialog;
+import static javax.swing.JOptionPane.showInputDialog;
 
 //Todo:  add logic into the controller - error check for invalid card, move to withdraw screen for valid. 
 public class Check_Balance_Controller {
@@ -26,28 +27,20 @@ public class Check_Balance_Controller {
 	// Action Listener Events to be added here.
 	public void runController() {
 
-		//
-		// checkBa.getInsertCardButton().addActionListener(new ActionListener() {
-
-		// @Override
-		// public void actionPerformed(ActionEvent e) {
-		// // System.out.println(welcomeScreen.getCardNumber().getText());
-
-		// int card = Integer.parseInt(welcomeScreen.getCardNumber().getText());
-		// BankCard bc = cardDB.getCard(card);
-		// if (bc.getCardNumber() == 0000 && bc.getBalance() == 0000 &&
-		// bc.isAccountStatus() == false) {
-		// showMessageDialog(null, "The card is not valid");
-		// welcomeScreen.reset();
-		// } else {
-		// welcomeScreen.dispose();
-		// EnterPinScreen enterPinScreen = new EnterPinScreen(bc);
-		// enterPinScreen.displayFrame();
-		// }
-		// }
-
-		// });
 		checkBalance.setBalance(Double.toString(bc.getBalance()));
+		checkBalance.getDepositButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// System.out.println(welcomeScreen.getCardNumber().getText());
+				String dep = showInputDialog("How much do you want to deposit");
+				Double deposit = Double.parseDouble(dep);
+				Double newBalance = (bc.getBalance() + deposit);
+				bc.setBalance(newBalance);
+				checkBalance.setBalance(Double.toString(newBalance));
+
+			}
+		});
 	}
 
 }
