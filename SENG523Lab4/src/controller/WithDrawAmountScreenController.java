c182d4807ff720685bf03f96033665a9edcfee0d
kaziashfaq <40367281+kaziashfaq@users.noreply.github.com>
2021-12-05 18:56:38 -0700

    Return to balance page after withdraw

--- SENG523Lab4/src/controller/WithDrawAmountScreenController.java
+++ SENG523Lab4/src/controller/WithDrawAmountScreenController.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import view.Check_Balance;
 import view.WithDrawAmountScreen;
 
 import model.BankCard;
@@ -10,51 +11,72 @@ import model.BillDisburse;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 import static javax.swing.JOptionPane.showInputDialog;
-import javax.swing.*; 
+import javax.swing.*;
 
 // Remember to do checks for verify balance as per Lab 3. 
 public class WithDrawAmountScreenController {
 
 	private WithDrawAmountScreen withdrawAmountScreen;
-	
+
 	public WithDrawAmountScreenController(WithDrawAmountScreen withdrawAmountScreen) {
 		this.withdrawAmountScreen = withdrawAmountScreen;
 	}
-	
+
 	public void reset() {
 		withdrawAmountScreen.dispose();
 		ATMProject.start();
 	}
+
 	public void runController() {
-		
-		// Enter 
+
+		// Enter
 		withdrawAmountScreen.getEnterButton().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				// Todo: Logic and checking if funds available. If not, display error screen.
 				System.out.println(withdrawAmountScreen.getWithdrawField().getText());
 				double withdrawamount = Integer.parseInt(withdrawAmountScreen.getWithdrawField().getText());
+
 				if (withdrawamount <= withdrawAmountScreen.getAmountInAccount()) {
-					
-					while (withdrawamount > 0){
-					if (withdrawamount >= 100){
-						showMessageDialog(null,"$100 dollars succesfully provided","Payment",JOptionPane.WARNING_MESSAGE);
-						withdrawamount = withdrawamount - 100;continue;}
-					if (withdrawamount >= 50){
-						showMessageDialog(null,"$50 dollars succesfully provided","Payment",JOptionPane.WARNING_MESSAGE);
-						withdrawamount = withdrawamount - 50;continue;}
-					if (withdrawamount >= 20){
-						showMessageDialog(null,"$20 dollars succesfully provided","Payment",JOptionPane.WARNING_MESSAGE);
-						withdrawamount = withdrawamount - 20;continue;}
-					if (withdrawamount >= 10){
-						showMessageDialog(null,"$10 dollars succesfully provided","Payment",JOptionPane.WARNING_MESSAGE);
-						withdrawamount = withdrawamount - 10;continue;}
-					if (withdrawamount >= 5){
-						showMessageDialog(null,"$5 dollars succesfully provided","Payment",JOptionPane.WARNING_MESSAGE);
-						withdrawamount = withdrawamount - 5;continue;}
+					double newBalance = withdrawAmountScreen.getAmountInAccount() - withdrawamount;
+					withdrawAmountScreen.getCard().setBalance(newBalance);
+					while (withdrawamount > 0) {
+						if (withdrawamount >= 100) {
+							showMessageDialog(null, "$100 dollars succesfully provided", "Payment",
+									JOptionPane.WARNING_MESSAGE);
+							withdrawamount = withdrawamount - 100;
+							continue;
+						}
+						if (withdrawamount >= 50) {
+							showMessageDialog(null, "$50 dollars succesfully provided", "Payment",
+									JOptionPane.WARNING_MESSAGE);
+							withdrawamount = withdrawamount - 50;
+							continue;
+						}
+						if (withdrawamount >= 20) {
+							showMessageDialog(null, "$20 dollars succesfully provided", "Payment",
+									JOptionPane.WARNING_MESSAGE);
+							withdrawamount = withdrawamount - 20;
+							continue;
+						}
+						if (withdrawamount >= 10) {
+							showMessageDialog(null, "$10 dollars succesfully provided", "Payment",
+									JOptionPane.WARNING_MESSAGE);
+							withdrawamount = withdrawamount - 10;
+							continue;
+						}
+						if (withdrawamount >= 5) {
+							showMessageDialog(null, "$5 dollars succesfully provided", "Payment",
+									JOptionPane.WARNING_MESSAGE);
+							withdrawamount = withdrawamount - 5;
+							continue;
+						}
 					}
-					withdrawAmountScreen.dispose(); 
+					showMessageDialog(null, "Transaction was successful.");
+					withdrawAmountScreen.dispose();
+					Check_Balance checkBalance = new Check_Balance(withdrawAmountScreen.getCard());
+					checkBalance.displayFrame();
 				} else {
 					showMessageDialog(null,
 							"Not enough funds for withdrawel, Please restart transaction.");
@@ -62,17 +84,16 @@ public class WithDrawAmountScreenController {
 				}
 			}
 		});
-		
-		
+
 		// Cancel.
 		withdrawAmountScreen.getCancelButton().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				withdrawAmountScreen.dispose();
-				//Open a new Welcome Screen? 
+				// Open a new Welcome Screen?
 			}
 		});
-		
+
 	}
-	
+
 }
